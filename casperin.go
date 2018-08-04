94de60cd2fb656e398e6e6797c645f63647e62b0
David-Orson <onlyorson@outlook.com>
2018-08-04 14:07:23 +0200

    UCI

--- casperin.go
+++ casperin.go
@@ -1,29 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/David-Orson/casperin/backend"
 	"github.com/David-Orson/casperin/engine"
+	"github.com/David-Orson/casperin/uci"
 )
 
 func main() {
-	pos := backend.InitialPosition
-	var child backend.Position
-	pos.Print()
-	for i := 0; i < 90; i++ {
-		start := time.Now()
-		end := time.After(30 * time.Second)
-		move := engine.Search(&pos, end)
-		move.Inspect()
-		if move == 0 {
-			return
-		}
-		pos.MakeMove(move, &child)
-		pos = child
-		fmt.Println(time.Now().Sub(start))
-		pos.Print()
-		fmt.Println("")
-	}
+	e := engine.Engine{}
+	uci := uci.NewUciProtocol(e)
+	uci.Run()
 }
