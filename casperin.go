3f826f6996a3d46bc5f3e2b06f4bbde5ea5b3af9
David-Orson <onlyorson@outlook.com>
2019-05-22 07:35:27 +0200

    Tuning results

--- casperin.go
+++ casperin.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/David-Orson/casperin/engine"
+	"github.com/David-Orson/casperin/evaluation"
 	"github.com/David-Orson/casperin/uci"
+	"os"
 )
 
 func main() {
+	if len(os.Args) > 0 && os.Args[1] == "tune" {
+		evaluation.Tune()
+		return
+	}
 	e := engine.NewEngine()
 	uci := uci.NewUciProtocol(e)
 	uci.Run()
